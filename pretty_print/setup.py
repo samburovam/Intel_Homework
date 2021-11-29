aa05235972082556dac28e7edcae108531f7c189
samburovam <samburova.2001@mail.ru>
2021-11-29 21:14:19 +0300

    Homework 2

---
+++ pretty_print/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="pp_package",
+    version="0.1",
+    description="Show current time",
+    author="Margarita Samburova",
+    packages=[
+        "time_namespace.pp_package",
+    ],
+    install_requires=[
+        "requests==2.26.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "get_time_pp=time_namespace.pp_package.pp_module:main",
+        ]
+    },
+    namespace_packages=['time_namespace']
+)
