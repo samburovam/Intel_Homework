26335948451c5e843aa0e4621f5555da988bb5b3
samburovam <55128869+samburovam@users.noreply.github.com>
2021-12-06 21:38:19 +0300

    clean up

--- pretty_print/time_namespace/pp_package/pp_module.py
+++
@@ -1,8 +0,0 @@
-from time_namespace.get_time_package.get_time_module import time
-from datetime import datetime
-
-def pretty_time():
-    formatted_time = datetime.fromtimestamp(time()).strftime("%Y-%m-%d %H:%M:%S")
-    return formatted_time
-def pretty_print():
-    print(pretty_time())
